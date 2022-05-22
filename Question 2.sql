61ea452ca1adb844a0da995e30b0c80d6e6de7ae
Ophelia Sin <76908733+opheliasin@users.noreply.github.com>
2022-05-22 16:49:23 -0700

    Update Question 2.sql

--- Question 2.sql
+++ Question 2.sql
@@ -1,5 +1,9 @@
 -- Question 2A
 -- How many orders were shipped by Speedy Express in total?
+-- EXPLANATION: First, I selected the two columns of interest, which are the Shipper's name and the total orders for each shipper.
+-- The total order for each shipper is aggregated from the Orders table using the Count(*) function. I then did an inner join between the newly created
+-- table OrderCount and Shippers on the ShipperID. Finally, I used WHERE to only output when the shipper is 'Speedy Express'.
+
 SELECT Shippers.ShipperName, OrderCount.TotalOrders
 FROM (
     SELECT
@@ -18,7 +22,11 @@ WHERE Shippers.ShipperName = 'Speedy Express'
 
 
 -- Question 2B
---What is the last name of the employee with the most orders?
+-- What is the last name of the employee with the most orders?
+-- EXPLANATION: First, I selected the column LastName from the Employees table. I then created a new table called OrdersByEmployee which 
+-- shows the number of orders for each employee. Next, the new table is joined with the Employees table on the Employee ID. Finally, I ordered the table
+-- in descending order and limited the output to return only the last name of the employee with the most orders.
+
 SELECT 
     Employees.LastName
 FROM 
@@ -29,18 +37,21 @@ FROM
         Orders 
     GROUP BY 
         EmployeeID)
-    AS OrderByEmployee
+    AS OrdersByEmployee
 INNER JOIN
     Employees
-ON OrderByEmployee.EmployeeID = Employees.EmployeeID
+ON OrdersByEmployee.EmployeeID = Employees.EmployeeID
 ORDER BY 
-    OrderByEmployee.TotalOrders DESC
+    OrdersByEmployee.TotalOrders DESC
     LIMIT 1
 --ANSWER: Peacock
 
 
---Question 2C
---What product was ordered the most by customers in Germany?
+-- Question 2C
+-- What product was ordered the most by customers in Germany?
+-- EXPLANATION: I first selected the Product's name from the Product table. Then, I joined Order Details and Products on the ProductID field. The WHERE 
+-- statement consists of two nested queries, in which only orders made by customers within Germany in the Orders table will be selected in the 
+-- OrderDetails table. Finally, the results are grouped by ProductName and ordered by the total number of orders made per product.
 
 SELECT 
     Products.ProductName 
@@ -63,7 +74,7 @@ WHERE OrderDetails.OrderID IN
             COUNTRY = 'Germany')    
     )
 GROUP BY 
-    OrderDetails.ProductID, Products.ProductName
+    Products.ProductName 
 ORDER BY 
     SUM(OrderDetails.Quantity) DESC
 LIMIT 1
